e0fdd8632af6f0283226b8c316a09a53a56780ed
Ivanka Kirinova <77276607+DesperateDreamer@users.noreply.github.com>
2025-03-19 20:49:33 +0200

    Add configuration method to program

--- WalletMate.API/Program.cs
+++ WalletMate.API/Program.cs
@@ -1,6 +1,9 @@
+using WalletMate.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddUserSecrets<Program>();
+builder.ConfigureInAppServices();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
