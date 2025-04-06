c55f9c3436ab88ef4f94eefba8fe91faaa04e4ea
Ivanka Kirinova <77276607+DesperateDreamer@users.noreply.github.com>
2025-04-06 20:31:28 +0300

    Remove generic TKey

--- WalletMate.BLL.Shared/CacheManager.cs
+++ WalletMate.BLL.Shared/CacheManager.cs
@@ -2,45 +2,45 @@ using System.Collections.Concurrent;
 
 namespace WalletMate.BLL.Shared;
 
-public sealed class CacheManager<TKey, TValue> where TKey : notnull
+public sealed class CacheManager<TValue>
 {
     private CacheManager()
     {
     }
     
-    private static readonly Lazy<CacheManager<TKey, TValue>> _instance =
-        new(() => new CacheManager<TKey, TValue>());
+    private static readonly Lazy<CacheManager<TValue>> _instance =
+        new(() => new CacheManager<TValue>());
     
-    public static CacheManager<TKey, TValue> Instance => _instance.Value;
+    public static CacheManager<TValue> Instance => _instance.Value;
     
-    private readonly ConcurrentDictionary<TKey, TValue?> _cache = new();
+    private readonly ConcurrentDictionary<Guid, TValue?> _cache = new();
     
-    public TValue? GetOrAdd(TKey key, Func<TValue> factory)
+    public TValue? GetOrAdd(Guid key, Func<TValue> factory)
     {
         return _cache.GetOrAdd(key, _ => factory());
     }
 
-    public bool TryGetValue(TKey key, out TValue? value)
+    public bool TryGetValue(Guid key, out TValue? value)
     {
         return _cache.TryGetValue(key, out value);
     }
 
-    public void Set(TKey key, TValue? value)
+    public void Set(Guid key, TValue? value)
     {
         _cache[key] = value;
     }
 
-    public bool Remove(TKey key)
+    public bool Remove(Guid key)
     {
         return _cache.TryRemove(key, out _);
     }
 
-    public IEnumerable<KeyValuePair<TKey, TValue?>> GetAll()
+    public IEnumerable<KeyValuePair<Guid, TValue?>> GetAll()
     {
         return _cache.ToArray();
     }
 
-    public bool ContainsKey(TKey key)
+    public bool ContainsKey(Guid key)
     {
         return _cache.ContainsKey(key);
     }
